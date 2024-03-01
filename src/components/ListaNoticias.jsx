300388d093960052e60a86084001b3e703744ac2
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-01 20:10:17 -0300

    se agrega maquetado de la noticia que será mostrada por la API

--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -1,10 +1,20 @@
 import { Col, Row } from "react-bootstrap"
+import Noticia from "./Noticia"
 
 const ListaNoticias = () => {
   return (
-    <Row>
+    <Row className="my-4">
       <Col sm={6} md={4} lg={3}>
-        <div>Noticia</div>
+        <Noticia />
+      </Col>
+      <Col sm={6} md={4} lg={3}>
+        <Noticia />
+      </Col>
+      <Col sm={6} md={4} lg={3}>
+        <Noticia />
+      </Col>
+      <Col sm={6} md={4} lg={3}>
+        <Noticia />
       </Col>
     </Row>
   )
---
+++ src/components/Noticia.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Noticia = () => {
+  return (
+    <>
+      <h2>Título de la noticia</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto error vel optio, numquam dicta voluptates fuga blanditiis laboriosam debitis repellat deleniti suscipit atque repellendus dolores molestias. Consequuntur corporis aut reiciendis.</p>
+    </>
+  )
+}
+
+export default Noticia
