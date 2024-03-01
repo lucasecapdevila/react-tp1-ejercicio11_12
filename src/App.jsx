07507487229a23a24b14b3861de2d2504e61ee1a
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-01 18:55:11 -0300

    se añade footer al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import BarraNavegacion from './components/common/BarraNavegacion';
+import Footer from './components/common/Footer';
 
 function App() {
   return (
     <>
       <BarraNavegacion />
+      <Footer />
     </>
   )
 }
