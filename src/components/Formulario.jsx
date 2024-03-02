e07233498ed94c098ceea528aa2b1f5fe82be029
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-02 20:17:30 -0300

    se agrega función para llamar a la API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react"
 import { Button, Form } from "react-bootstrap"
 
 const Formulario = () => {
+  useEffect(() => {  
+    consultarAPI()
+  }, [])
+
+  const consultarAPI = async() => {
+    try {
+      const respuesta = await fetch('https://newsdata.io/api/1/news?apikey=pub_39181df8f313285099388f00a02da90ef9161')
+      const news = await respuesta.json()
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <Form>
       <Form.Group controlId="formCategoria" className="mb-3">
